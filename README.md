489ff790d5ec8b72adc4d4fffe6e3d2c50956781
Giovanni Ceroni <at0mdud3@gmail.com>
2022-06-13 10:08:18 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# piBot
+A functional Discord bot for your Raspberry Pi.
+
+
+# Commands
+```/temp``` ---> Shows your Pi temperature
+```/shutdown``` ---> Reboots or halts your Pi
---
+++ main.py
@@ -0,0 +1,89 @@
+# (c) 2022 th3atom
+# Protected by AGNUv3
+# piBot: A Discord bot for managing your Raspberry Pi
+
+# Load Modules
+import interactions, subprocess, os
+from dotenv import load_dotenv
+
+# Initialize Bot and .env file
+load_dotenv()
+UID = os.getenv("UID")
+TOKEN = os.getenv("TOKEN")
+bot = interactions.Client(token=TOKEN)
+
+# buttons and rows
+reboot = interactions.Button(
+    style=interactions.ButtonStyle.DANGER,
+    label="Reboot",
+    custom_id="reb"
+)
+
+halt = interactions.Button(
+    style=interactions.ButtonStyle.DANGER,
+    label="Halt",
+    custom_id="hal"
+)
+
+shucanc = interactions.Button(
+    style=interactions.ButtonStyle.SECONDARY,
+    label="Cancel",
+    custom_id="shucancbut"
+)
+
+shurow = interactions.ActionRow(
+    components=[reboot, halt, shucanc]
+)
+
+@bot.command(
+    name="temp",
+    description="Outputs temperature of the Pi"
+)
+async def temp(ctx: interactions.CommandContext):
+    if ctx.user.id == UID:
+        cout = subprocess.run(["/usr/bin/vcgencmd", 'measure_temp'], stdout=subprocess.PIPE)
+        cout = cout.stdout
+        cout = cout.decode('utf-8')
+        cout = cout[5:]
+        await ctx.send("🌡️ Temperature: " + cout)
+    else:
+        pass
+
+@bot.command(
+    name="shutdown",
+    description="Halts or reboots the Pi",
+)
+async def shutdown(ctx: interactions.CommandContext):
+    if ctx.user.id == UID:
+        msg1 = await ctx.send("What would you like to do?", components=shurow, ephemeral=True)
+    else:
+        pass
+
+@bot.component("reb")
+async def button_response(ctx):
+    if ctx.user.id == UID:
+        await ctx.edit("⌛ Rebooting...", components=None)
+        os.system("reboot")
+    else:
+        pass
+
+@bot.component("hal")
+async def button_response(ctx):
+    if ctx.user.id == UID:
+        await ctx.edit("⌛ Halting the Pi...", components=None)
+        os.system("halt")
+    else:
+        pass
+
+@bot.component("shucancbut")
+async def button_response(ctx):
+    if ctx.user.id == UID:
+        await ctx.edit("Interaction cancelled!", components=None)
+    else:
+        pass
+
+try:
+    bot.start()
+except KeyboardInterrupt:
+    print("Shutting down bot...")
+    exit()
